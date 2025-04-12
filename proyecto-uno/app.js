786ba5ce5a8917ab66d5cb41279e42add741ebf9
Sandra Castro España <sandracaes00@gmail.com>
2025-04-11 21:50:22 -0600

    Se sube la práctica terminada del proyecto 1 de módulo 4, en la que se agregan especificaciones de ECMAScript 6

--- proyecto-uno/app.js
+++ proyecto-uno/app.js
@@ -2,9 +2,10 @@
 import { registrarDestino, mostrarItinerario } from './viajes.js';
 
 // Iniciar la aplicación
-function iniciarApp() {
+// Cuarta función flecha (=>)
+const iniciarApp = () => {
     // Ejemplo de cómo registrar destinos
-    registrarDestino("Paris", "2025-09-", "Avión");
+    registrarDestino("Paris", "2025-09-29", "Avión");
     registrarDestino("New York", "2025-06-13", "Autobús");
     registrarDestino("Brasil", "2025-03-22", "Tren");
     registrarDestino("Japón", "2025-06-01", "Avión");
--- proyecto-uno/viajes.js
+++ proyecto-uno/viajes.js
@@ -3,22 +3,22 @@
 const destinos = [];
 
 // Función para registrar un destino de viaje
-function registrarDestino(destino, fecha, transporte) {
-    // TODO: Crear un objeto con los datos del destino
-    // Se modifica "var" por "const" porque la variable no va a cambiar aunque sí el valor del contenido
-    const nuevoViaje = {
-        destino: destino,
-        fecha: fecha,
-        transporte: transporte,
-        costo: calcularCosto(destino, transporte)
-    };
+// Primera función flecha (=>), no se usa "function", pero significa que hay una función almacenada en una variable
+const registrarDestino = (destino, fecha, transporte) => {
+const nuevoViaje = {
+    destino,
+    fecha,
+    transporte,
+    costo: calcularCosto(destino, transporte),
+};
 
     destinos.push(nuevoViaje);
-}
+};
 
+// Se agregan destinos además de los propuestos
 // Función para calcular el costo del viaje
-function calcularCosto(destino, transporte) {
-    // Se modifica "var" por "let" porque la variable si puede cambiar
+// Segunda función flecha (=>)
+const calcularCosto = (destino, transporte) => {
     let costoBase = 0;
 
     // Costo base por destino
@@ -48,17 +48,17 @@ function calcularCosto(destino, transporte) {
 }
 
 // Función para mostrar el itinerario de los viajes registrados
-function mostrarItinerario() {
-    // TODO: Recorrer el arreglo de destinos y mostrar la información de cada uno
-    // se modificar por por "let" y "const" respetivamente de acuerdo a los cambios en la variable 
-    for (let i = 0; i < destinos.length; i++) {
-        const viaje = destinos[i];
-        console.log("Destino: " + viaje.destino);
-        console.log("Fecha: " + viaje.fecha);
-        console.log("Transporte: " + viaje.transporte);
-        console.log("Costo: $" + viaje.costo);
-        console.log("---------------------------");
-    }
+// Tercera función flecha (=>)
+const mostrarItinerario = () => {
+// Método de los arreglos que ejecuta una función en cade elemento del arreglo
+   destinos.forEach((viaje) => {
+    console.log(`Destino: ${viaje.destino}`);
+    console.log(`Fecha: ${viaje.fecha}`);
+    console.log(`Transporte: ${viaje.transporte}`);
+    console.log(`Costo: $${viaje.costo}`);
+    console.log("-----------------------");
+    
+});
 }
 
 // Exportar funciones
